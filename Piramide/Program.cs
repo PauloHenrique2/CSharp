b6b1c67d51ff67e3923d8711fdb2a8982da9ab14
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-03-10 15:34:35 -0300

    Novo projeto adicionado
    Projeto que gera uma pirâmide de asteriscos de acordo com a entrada do usuário (feito com loop For)

---
+++ Piramide/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piramide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int linhas, l, i;
+
+            Console.Write("Digite o nº de linhas: ");
+            linhas = Int32.Parse(Console.ReadLine());
+
+            for (l = 0; l < linhas; l++)  // Contagem das Linhas
+            {
+                for (i = 0; i < linhas - l - 1; i++)  // Geração dos Espaços
+                {
+                    Console.Write(" ");
+                }
+
+                for (i = 0; i < 2 * l + 1; i++)  // Geração dos Asteriscos
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
