de0f66236dcbb597d1ff119303c1cf7fb6e55cfb
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-03-17 13:25:50 -0300

    Programa em console feito em C#
    Programa que calcula o salário bruto de um professor, utilizando conceitos de POO (encapsulamento) e classe

---
+++ Exercicio-Classe_Professor/Professor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Classe_Professor
+{
+    internal class Professor
+    {
+        private string nome;
+        private double salHora;
+        private int horasTrab, anosTrab;
+
+        public Professor(string n, double sh, int ht, int at)
+        {
+            this.nome = n;
+            this.salHora = sh;
+            this.horasTrab = ht;
+            this.anosTrab = at;
+        }
+
+        public string Getnome()
+        {
+            return this.nome;
+        }
+
+        public double GetSalBruto()
+        {
+            return (this.salHora * this.horasTrab * 4.5) + ((this.salHora * this.horasTrab * 4.5 ) * (this.anosTrab/5) * 0.05);
+        }
+
+        public string GetDados()
+        {
+            return $"Nome: {this.nome}\n Salário bruto: {GetSalBruto():C}";
+        }
+    }
+}
---
+++ Exercicio-Classe_Professor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Classe_Professor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor p = new Professor("Pedro",38.23,20,17);
+            Console.WriteLine(p.GetDados());
+            Console.ReadKey();
+        }
+    }
+}
