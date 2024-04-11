ec38ce5ec92d2ee87f7d07d2ddf19aeeb00437a1
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-04-11 16:42:42 -0300

    Novo programa gerador de matriz adicionado
    Programa que gera uma matriz 6x6 com números aleatórios e calcula a soma e a média dos elementos acima da diagonal secundária.

---
+++ Matriz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat = new int[6, 6];
+            Random r = new Random();
+            int soma = 0;
+            double media = 0;
+
+            /* Soma e média dos elementos acima da diagonal secundária */
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (j < mat.GetLength(1) - i)
+                    {
+                        mat[i, j] = r.Next(0, 10);
+                        Console.Write(mat[i, j] + " ");
+                        soma += mat[i, j];
+                    }
+                    media = soma / 21;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.Write("A soma dos elementos acima da diagonal secundária é: " + soma + "\n");
+            Console.Write("A média dos elementos acima da diagonal secundária é: " + media);
+
+            Console.ReadKey();
+        }
+    }
+}
