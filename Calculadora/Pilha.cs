1e4cf5355dd8faae9fc8411c87b2438d6445cc11
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-04-16 13:58:08 -0300

    Calculadora feita utilizando o tipo abstrato de dados pilha
    A calculadora realiza as operações básicas (+ - * /) de acordo com o input do usuário na linha de comando

---
+++ Calculadora/Pilha.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Classe Pilha - Implementa o tipo abstrato de dados pilha estática
+ * Operações: 
+ * Vazia() -> retorna true se a pilha está vazia.
+ * Cheia() -> returna true se a pilha está cheia.
+ * Empilhar(x) -> coloca o elemento x no topo da pilha.
+ * Desempilhar() -> retorna o elemento situado no topo da pilha.
+ */
+
+namespace Calculadora
+{
+    class Pilha
+    {
+        private int[] vet; // Vetor para armazenar os elementos da pilha
+        private int topo; // Indica a posição do topo
+
+        public Pilha(int tam) // Construtor
+        { 
+            vet = new int[tam];
+            topo = 0;
+        }
+
+        public void Empilhar(int x)
+        {
+            vet[topo] = x;
+            topo++;
+        }
+
+        public int Desempilhar()
+        {
+            topo--;
+            return (vet[topo]);
+        }
+
+        public bool Vazia()
+        {
+            return (topo == 0);
+        }
+
+        public bool Cheia()
+        {
+            return (topo == vet.Length);
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,92 @@
+﻿using ExemploPilha;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilha p = new Pilha(100);
+            string variavel = "";
+            
+            while (true)
+            {
+                Console.WriteLine("Insira um número ou operador: (digite 's' para sair)");
+                variavel = Console.ReadLine();
+
+                if (variavel == "+")
+                {
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!");}
+
+                    int num1 = p.Desempilhar();
+
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!"); }
+
+                    int num2 = p.Desempilhar();
+                    int soma = num1 + num2;
+                    p.Empilhar(soma);
+                    Console.WriteLine(soma);
+                }
+
+                else if (variavel == "-")
+                {
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!"); }
+
+                    int num1 = p.Desempilhar();
+
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!"); }
+
+                    int num2 = p.Desempilhar();
+                    int subtracao = num2 - num1;
+                    p.Empilhar(subtracao);
+                    Console.WriteLine(subtracao);
+                }
+
+                else if (variavel == "/")
+                {
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!"); }
+
+                    int num1 = p.Desempilhar();
+
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!"); }
+
+                    int num2 = p.Desempilhar();
+                    int divisao = num2 / num1;
+                    p.Empilhar(divisao);
+                    Console.WriteLine(divisao);
+                }
+
+                else if (variavel == "*")
+                {
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!"); }
+
+                    int num1 = p.Desempilhar();
+
+                    if (p.Vazia() == true) { Console.WriteLine("Pilha vazia!"); }
+                    
+                    int num2 = p.Desempilhar();
+                    int multiplicao = num1 * num2;
+                    p.Empilhar(multiplicao);
+                    Console.WriteLine(multiplicao);
+                }
+
+                else if (variavel == "s" || variavel == "S")
+                {
+                    Environment.Exit(0);
+                }
+
+                else
+                {
+                    p.Empilhar(int.Parse(variavel));
+                }
+            }
+        }
+    }
+}
