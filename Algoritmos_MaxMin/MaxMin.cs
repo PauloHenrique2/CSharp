c97da292ba8f0f1182243655ead0bcbc2023014d
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-04-29 16:01:48 -0300

    Algoritmos de contagem para análise de complexidade adicionados
    Programa que usa três métodos que recebem um vetor de números inteiros e calculam o menor e o maior valor contidos no vetor, para fazer a contagem de testes "if's" que ocorrem nos três métodos.

---
+++ Algoritmos_MaxMin/MaxMin.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_4
+{
+    class MaxMin
+    {
+        // Operação relevante: Testes (if)
+
+        // Complexidade: T(n) = 2n - 2
+        public int MaxMin1(int[] vet)
+        {
+            int ma = 0;
+            int me = 0;
+            int cont = 0;
+            ma = me = vet[0];
+            for (int i = 1; i < vet.Length; i++)
+            {
+                cont += 2;
+                if (vet[i] < me)
+                    me = vet[i];
+
+                if (vet[i] > ma)
+                    ma = vet[i];
+            }
+            return cont;
+            
+        }
+
+        // Complexidade:
+        // Melhor caso: T(n) = n - 1
+        // Pior caso: T(n) = 2n - 2
+        // Caso médio: T(n) = 3n/2 - 3/2
+        public int MaxMin2(int[] vet)
+        {
+            int ma = 0;
+            int me = 0;
+            int cont = 0;
+            ma = me = vet[0];
+            for (int i = 1; i < vet.Length; i++)
+            {
+                cont++;
+                if (vet[i] < me)
+                    me = vet[i];
+                else
+                {
+                    cont++;
+                    if (vet[i] > ma)
+                        ma = vet[i];
+                }
+                    
+                
+            }
+            return cont;
+        }
+
+
+        // Complexidade: T(n) = 3n/2 - 2
+        public int MaxMin3(int[] vet)
+        {
+            int ma = 0;
+            int me = 0;
+            int cont = 0;
+            cont++;
+            if (vet[0] < vet[1])
+            {
+                me = vet[0];
+                ma = vet[1];
+            }
+            else
+            {
+                me = vet[1];
+                ma = vet[0];
+            }
+            for (int i = 2; i < vet.Length; i += 2)
+            {
+                cont ++;
+                if (vet[i] < vet[i + 1])
+                {
+                    cont++;
+                    if (vet[i] < me)
+                        me = vet[i];
+                    cont++;
+                    if (vet[i + 1] > ma)
+                        ma = vet[i + 1];
+                }
+                else
+                {
+                    cont++;
+                    if (vet[i + 1] < me)
+                        me = vet[i + 1];
+                    cont++;
+                    if (vet[i] > ma)
+                        ma = vet[i];
+                }
+            }
+            return cont;
+        }
+    }
+}
---
+++ Algoritmos_MaxMin/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_4
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] vetCres = new int[1000];
+            int[] vetDecres = new int[1000];
+            int[] vetAleat = new int[1000];
+            Random r = new Random();
+            MaxMin m = new MaxMin();
+
+            for (int i = 0; i < vetCres.Length; i++)
+            {
+                vetCres[i] = i;
+            }
+
+            for (int i = 0; i < vetDecres.Length; i++)
+            {
+                vetDecres[i] = vetDecres.Length - i;
+            }
+
+            for (int i = 0; i < vetAleat.Length; i++)
+            {
+                vetAleat[i] = r.Next(0, 1000); 
+            }
+            Console.WriteLine("Contagem do vetor crescente: ");
+            Console.WriteLine(m.MaxMin1(vetCres));
+            Console.WriteLine(m.MaxMin2(vetCres));
+            Console.WriteLine(m.MaxMin3(vetCres) + "\n");
+
+
+            Console.WriteLine("Contagem do vetor decrescente: ");
+            Console.WriteLine(m.MaxMin1(vetDecres));
+            Console.WriteLine(m.MaxMin2(vetDecres));
+            Console.WriteLine(m.MaxMin3(vetDecres) + "\n");
+
+
+            Console.WriteLine("Contagem do vetor aleatório: ");
+            Console.WriteLine(m.MaxMin1(vetAleat));
+            Console.WriteLine(m.MaxMin2(vetAleat));
+            Console.WriteLine(m.MaxMin3(vetAleat));
+
+            Console.ReadKey();
+        }
+    }
+}
