c0b6eb7be9f62d7e7ff262428a88849fee481fe4
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-10-08 18:52:10 -0300

    Programa concatenador de data adicionado

---
+++ Concatenador_de_data/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concatenador_de_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Data d = new Data(1, 1, 2100);
+            Console.WriteLine(d.RetornaData());
+            Console.ReadKey();
+        }
+    }
+}
