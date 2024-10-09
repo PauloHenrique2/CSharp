d217d97747395f9f671f739fb46d05990fb1c2dc
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-10-08 23:14:44 -0300

    Implementação aprimorada do tipo abstrato lista encadeada

---
+++ Lista_encadeada_2.0/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_3
+{
+    internal class Program{
+
+        static void Main(string[] args){
+
+            Lista l = new Lista();
+            string nome;
+            string variavel;
+            int c = 0;
+
+            while (true){
+                Console.WriteLine
+                ("Escolha uma opção:" +
+                "\n 1 - Inserir" +
+                "\n 2 - Pesquisar" +
+                "\n 3 - Imprimir Lista" +
+                "\n 4 - Sair");
+
+                variavel = Console.ReadLine();
+
+                if (int.Parse(variavel) == 1){
+
+                    while (true){
+                        Console.Write("Digite um nº (-1 para sair): ");
+                        c = Convert.ToInt32(Console.ReadLine());
+
+                        if (c == -1)
+                            break;
+
+                        Console.Write("Agora digite um nome para essa chave (-1 para sair): ");
+                        nome = Console.ReadLine();
+
+                    
+                        if (l.Pesquisar(c) == null && nome != null){
+                            NoLista no = new NoLista(c, nome);
+                            l.Inserir(no);
+                        }
+
+                        else 
+                            Console.WriteLine("Erro! Chave inserida já resistrada!");
+                    }
+                }
+
+                else if (int.Parse(variavel) == 2){
+                    Console.Write("Digite um nº a ser pesquisado: ");
+                    c = Convert.ToInt32(Console.ReadLine());
+
+                    NoLista n = l.Pesquisar(c);
+                    if (n == null)
+                        Console.WriteLine("Valor não encontrado!");
+
+                    else
+                        Console.WriteLine("Achou: " + n.nome);
+
+                    Console.WriteLine("Deseja remover o nó pesquisado ? (digite 's' para remover)");
+                    string resp = Console.ReadLine();
+
+                    if (resp == "s")
+                        l.Remover(n.chave);
+                }
+
+                else if (int.Parse(variavel) == 3)
+                    l.Imprimir();
+
+
+                else if (int.Parse(variavel) == 4)
+                    Environment.Exit(0);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lista_encadeada_2.0/TiposAbstratosDeDados.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_3
+{
+        class NoLista
+        {
+            public int chave;
+            public string nome;
+            public NoLista prox;
+            public NoLista(int c, string n)
+            {
+                chave = c;
+                nome = n;
+                prox = null;
+            }
+        }
+
+        class Lista
+        {
+            private NoLista prim, ult;
+
+            public Lista()
+            {
+                prim = ult = null;
+            }
+
+            public void Inserir(NoLista item)
+            {
+                if (prim == null)
+                    prim = item;
+
+                else
+                    ult.prox = item;
+                ult = item;
+            }
+
+            public void Imprimir()
+            {
+                NoLista aux = prim;
+                while (aux != null)
+                {
+                    Console.WriteLine(aux.chave + "(" +  "Nome: " + aux.nome + ")");
+                    aux = aux.prox;
+                }
+            }
+
+            public NoLista Pesquisar(int c)
+            {
+                NoLista aux = prim;
+                while (aux != null && aux.chave != c)
+                {
+                    aux = aux.prox;
+                }
+                return aux;
+            }
+
+            public bool Remover(int c)
+            {
+                NoLista aux = prim, ant = null;
+                while (aux != null && aux.chave != c)
+                {
+                    ant = aux;
+                    aux = aux.prox;
+                }
+                if (aux != null)
+                {
+                    if (ant != null) // não é o primeiro
+                        ant.prox = aux.prox;
+                    else // é o primeiro
+                        prim = aux.prox;
+                    if (aux == ult) // é o último
+                        ult = ant;
+                    aux.prox = null; // desconecta o nó da lista
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
